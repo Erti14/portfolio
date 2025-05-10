44ae48222d71e1c2ace21d4b719fd437b0618b2e
Erti Muhja <ertmuh18@htl-shkoder.com>
2025-05-10 14:14:29 +0200

    Trying to upload

--- astro.config.ts
+++ astro.config.ts
@@ -28,6 +28,7 @@ import rehypeUnwrapImages from "rehype-unwrap-images";
 // https://astro.build/config
 export default defineConfig({
 	site: siteConfig.url,
+	base: '/',
 	image: {
 		domains: ["webmention.io"],
 	},
