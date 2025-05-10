6cd2de184b87002187261932526cba1e98dbc13c
Erti Muhja <ertmuh18@htl-shkoder.com>
2025-05-10 13:52:13 +0200

    Update

--- astro.config.ts
+++ astro.config.ts
@@ -21,6 +21,10 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeExternalLinks from "rehype-external-links";
 import rehypeUnwrapImages from "rehype-unwrap-images";
 
+
+
+
+
 // https://astro.build/config
 export default defineConfig({
 	site: siteConfig.url,
